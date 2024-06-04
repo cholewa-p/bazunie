80bf3f4333cce981fd4ad79f0167bb4c91011c32
Paweł Cholewa <pawel.cholewa@o2.pl>
2024-06-04 23:20:56 +0200

    changes in main site

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from student import student_route
 from lecturer import lecturer_route
+from enrollment import enrollment_route
 app = Flask(__name__)
 app.register_blueprint(student_route)
 app.register_blueprint(lecturer_route)
+app.register_blueprint(enrollment_route)
 #Route to main page
 @app.route('/')
 def index():
@@ -11,5 +13,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
